62e5a36a7b9870d9fcb0964ddecf414c3ad2113b
Kevin Lo <klo74@my.bcit.ca>
2018-10-12 13:44:42 -0700

    Initial commit

---
+++ animal.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Owntrolf on 2018-10-12.
+//
+
+#ifndef COMP3512LAB4_ANIMAL_HPP
+#define COMP3512LAB4_ANIMAL_HPP
+
+
+class animal {
+    
+};
+
+
+#endif //COMP3512LAB4_ANIMAL_HPP
